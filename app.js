4056943f17edbc3bd2c04adfac0dc8b34b7b5055
Praveen Kumar <praveenkumarthipparam@gmail.com>
2023-03-21 00:04:19 -0500

    Template Strings

--- app.js
+++ app.js
@@ -1,41 +1,24 @@
-// var, let , const
-// let, const - blocked scoped {} - anything within {}
-// var - function scoped
+// Template Strings/Literals - easy to write
 
-console.log(amount);
+const firstName = "bob";
+const lastName = "sanders";
+const age = 25;
 
-// global scope
-var amount = 100;
+/* String Concatenation */
+const phrase =
+  "My full name is " +
+  firstName +
+  " " +
+  lastName +
+  " and I'm " +
+  age +
+  " years old.";
 
-// function scoped - global can't access local
-function calculate() {
-  // local scope
-  var random = "dollars";
-  console.log(`Hello, the amount is ${amount} ${random}`);
-}
-calculate();
-// can't access local
-// console.log(random);
+console.log(phrase);
 
-// if not function scoped i.e block scoped, global can access local value if it has same variable names which will cause problems
-var total = 100;
-var value = true;
+/* Template String/Literal */
+const person = `Hello, My full name is ${firstName.toUpperCase()} ${lastName.toUpperCase()} and I am ${
+  age * 2
+} years old.`;
 
-// after lines of code, since user writing functionality which should use global total, instead of number he will get string(name collisions)
-if (value) {
-  var total = "oranges";
-  console.log(`Hello, the total is ${total}`);
-}
-console.log(`what is the total: ${total}`);
-
-// global scope
-let number = 1000;
-{
-  // local scope
-  let number = 500;
-}
-{
-  let number = 1200;
-}
-
-console.log(number);
+console.log(person);
