af8605ca8ad3b11a2cbb8b5cf208d019a27bb8ca
Praveen Kumar <praveenkumarthipparam@gmail.com>
2023-03-25 00:00:38 -0500

    Swap Variables

--- app.js
+++ app.js
@@ -1,22 +1,15 @@
-// Destructuring
+// Destructuring - Swap Variables
 // faster/easier way to access/unpack values from arrays
 // objects into variables
 // Arrays
 
-const fruits = ["orange", "apple", "lemon"];
-const friends = ["john", "anna", "bob", "peter", "kelly"];
+let first = "john";
+let second = "bob";
 
-/* Prior to ES6 */
-const orange = fruits[0];
-const apple = fruits[1];
-const lemon = fruits[2];
+// const temp = first;
+// first = second;
+// second = temp;
 
-console.log(orange, apple, lemon);
+[second, first] = [first, second];
 
-/* ES6 */
-// const [friend, anna, bob, peter, kelly] = friends;
-// console.log(friend, anna, bob);
-
-// select some or skip others
-const [friend, , bob, , kelly] = friends;
-console.log(friend, bob, kelly);
+console.log(first, second);
