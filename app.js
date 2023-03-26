c57bc6cab243e66ddc9a268faac2436b3c66c1af
Praveen Kumar <praveenkumarthipparam@gmail.com>
2023-03-26 13:14:35 -0500

    New String Methods

--- app.js
+++ app.js
@@ -1,27 +1,35 @@
-// Destructuring
-// faster/easier way to access/unpack values from arrays
-// objects into variables
-// As Function Arguments
+// new string methods
+// startsWith(), endsWith(), includes(),repeat()
 
-const bob = {
-  first: "bob",
-  last: "sanders",
-  city: "san fransico",
-  siblings: {
-    sister: "jane",
-    brother: "peter",
-  },
+const person = "Peter Smith";
+const employee = "23456-EMP-PETER-SMITH";
+const manager = "23456-MAN-JOHN-DOE";
+
+/* startsWith() */
+console.log("=== startsWith() ===");
+console.log(person.startsWith("Pet"));
+// case-sensitive - since it is a string
+console.log(person.startsWith("PETER"));
+console.log(employee.startsWith("EMP", 6));
+
+/* endsWith() */
+console.log("=== endsWith() ===");
+console.log(manager.endsWith("DOE"));
+console.log(manager.endsWith("MAN", 9));
+
+/* includes() */
+console.log("=== includes() ===");
+console.log(employee.includes("PETER"));
+console.log(employee.includes("pETER"));
+
+/* repeat() */
+console.log("=== repeat() ===");
+console.log(person.repeat(2));
+
+// with Arrow Function
+const multiplyPeople = (person, amount = 5) => {
+  return person.repeat(amount);
 };
 
-/* Prior to ES6 */
-// function printPerson(person) {
-//   console.log(person.first);
-// }
-
-/* ES6 */
-function printPerson({ first, last, city }) {
-  // const { first, last, city } = person;
-  console.log(first, last, city);
-}
-
-printPerson(bob);
+const people = multiplyPeople(person, 10);
+console.log(people);
