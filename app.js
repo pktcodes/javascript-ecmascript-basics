b4d98920c6bd74ac2172faa17aa1de3ebc7d1694
Praveen Kumar <praveenkumarthipparam@gmail.com>
2023-03-26 15:21:36 -0500

    Spread Operator - Objects

--- app.js
+++ app.js
@@ -1,28 +1,10 @@
 // Spread Operator ...
-// Allows an iterable to spread/expand individually inside receiver
+// Allows an iterable to spread/expand individually inside reciever
 // Split into single items and copy them.
+// ES2018 - ES8 - Objects
 
-/* String are iterable too */
-const udemy = "udemy";
+const person = { name: "john", job: "developer" };
+const newPerson = { ...person, city: "chicago", name: "peter" };
 
-/* Arrays */
-const boys = ["john", "bob", "peter", "kelly"];
-const girls = ["anna", "susan"];
-const bestFriend = "arnold";
-
-const friends = [...boys, bestFriend, ...girls];
-console.log("friends: ", friends);
-
-// Never Reference them directly, since it will mutate original array
-// const newFriends = friends;
-// newFriends[0] = "katrina";
-
-// console.log("newFriends ", newFriends);
-// console.log("friends got mutated: ", friends);
-
-// Always copy them, will create new and not mutate to original
-const newFriends = [...friends];
-newFriends[0] = "katrina";
-
-console.log("newFriends:", newFriends);
-console.log("friends:", friends);
+console.log("person: ", person);
+console.log("newPerson: ", newPerson);
