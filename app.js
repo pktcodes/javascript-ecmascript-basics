0847f893fd9769291ae1a277758156a647600ece
Praveen Kumar <praveenkumarthipparam@gmail.com>
2023-03-25 23:27:03 -0500

    Object Destructuring

--- app.js
+++ app.js
@@ -1,15 +1,31 @@
-// Destructuring - Swap Variables
+// Destructuring
 // faster/easier way to access/unpack values from arrays
 // objects into variables
-// Arrays
+// Objects
 
-let first = "john";
-let second = "bob";
+const bob = {
+  first: "bob",
+  last: "sanders",
+  city: "san fransico",
+  siblings: {
+    sister: "jane",
+    brother: "peter",
+  },
+};
 
-// const temp = first;
-// first = second;
-// second = temp;
+/* Prior to ES6 */
+// const firstName = bob.first;
+// const lastName = bob.last;
+// const sister = bob.siblings.sister;
+// console.log(firstName, lastName, sister);
 
-[second, first] = [first, second];
+/* ES6 */
+const {
+  first: firstName,
+  last,
+  city,
+  zip,
+  siblings: { sister: favoriteSibling },
+} = bob;
 
-console.log(first, second);
+console.log(firstName, last, city, zip, favoriteSibling);
