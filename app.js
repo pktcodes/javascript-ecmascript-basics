f0f095cc21f1c5d59d4553ab01bdb3a4f3f1327f
Praveen Kumar <praveenkumarthipparam@gmail.com>
2023-03-30 23:02:51 -0500

    "for in" - Loop

--- app.js
+++ app.js
@@ -1,41 +1,13 @@
-// find - gets the specific item
-// findIndex - gets the index of the item
-// every - every item in the array needs to meet condition
-// some -  atleast one item
+// "for in" loop - iterate over object properties
+// not advised to use it on arrays, especially if the order is important
+// on arrays use "for of" loop instead
 
-const people = [
-  { id: 0, name: "peter" },
-  { id: 1, name: "susan" },
-  { id: 2, name: "anna" },
-];
+const person = {
+  name: "anna",
+  age: 20,
+  location: "chicago",
+};
 
-/* To get Anna - But filter returns an array,so need index to access */
-console.log("=== filter ===");
-const anna = people.filter((person) => person.name === "anna");
-console.log("Filter Anna: ", anna);
-console.log("Filter Anna: Name:", anna[0].name);
-
-/* find - returns an item*/
-console.log("=== find ===");
-const searchAnna = people.find((person) => person.name === "anna");
-console.log("searchAnna: ", searchAnna);
-console.log("searchAnnaName: ", searchAnna.name);
-
-/* findIndex  - return the index of the item*/
-console.log("=== findIndex ===");
-const searchAnnaIndex = people.findIndex((person) => person.name === "anna");
-console.log("SearchAnnaIndex: ", searchAnnaIndex);
-console.log("SearchAnnaIndexSliced: ", people.slice(0, searchAnnaIndex));
-
-const grades = ["A", "B", "A", "B", "C"];
-const goodGrades = ["A", "B", "A", "B"];
-
-/* every - return true if all the items accept the search criteria*/
-console.log("=== every ===");
-const allGoodGrades = goodGrades.every((grade) => grade !== "C");
-console.log("allGoodGrades: ", allGoodGrades);
-
-/* some - returns true if one item is found within search criteria*/
-console.log("=== some ===");
-const oneBadGrade = goodGrades.some((grade) => grade === "C");
-console.log("oneBadGrade: ", oneBadGrade);
+for (const propertyName in person) {
+  console.log(`${propertyName}: ${person[propertyName]}`);
+}
