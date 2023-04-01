ca24f7c564cf7c2b9ec22ae0ce5b4880edec5bed
Praveen Kumar <praveenkumarthipparam@gmail.com>
2023-04-01 00:22:41 -0500

    Array - includes()

--- app.js
+++ app.js
@@ -1,27 +1,10 @@
-// String includes() - checks if a string contains another string.
+// Array includes() - checks if a string contains another string.
+// Array includes - checks if the item is an array
 
-console.log("=== Case Sensitive ===");
-const product = {
-  title: "Leather Chair",
-};
-console.log(product.title.includes("le"));
+const groceries = ["milk", "apple", "banana", "carrot"];
 
-console.log("=== 2nd Argument ===");
-const firstName = "peter";
-const result = firstName.includes("pe", 0);
-console.log(result);
+let item = "lemon";
+item = "milk";
 
-/* Products */
-const products = [
-  { title: "Modern Poster" },
-  { title: "Bar Stool" },
-  { title: "Armchair" },
-  { title: "Leather Chair" },
-];
-
-const searchTitle = "a";
-
-const filteredProducts = products.filter((item) =>
-  item.title.toLowerCase().includes(searchTitle)
-);
-console.log(filteredProducts);
+const isItemIncluded = groceries.includes(item, 1);
+console.log(isItemIncluded);
